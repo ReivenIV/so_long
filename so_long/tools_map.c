46cc54f01b3592a382d058c8485d4d2fba586dbf
ReivenIV <runyalen88@duck.com>
2025-02-20 17:25:33 +0100

    tools map / player created

---
+++ so_long/tools_map.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bobytrap <bobytrap@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 17:08:43 by bobytrap          #+#    #+#             */
+/*   Updated: 2025/02/20 17:08:43 by bobytrap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// TODO count lines (will need GNL)
+// int	count_lines(char *av){}
+
+
+// is an strlen but in the context for these project i prefer to call it count_cols 
+int	count_cols(char *line)
+{
+	int	count;
+
+	count = 0;
+	while (line[i] != '\0')
+		count++;
+	return (count);
+}
+
---
+++ so_long/tools_player.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_player.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bobytrap <bobytrap@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 17:17:09 by bobytrap          #+#    #+#             */
+/*   Updated: 2025/02/20 17:17:09 by bobytrap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Will update the coordinates x:y of the player in the struct t_game.
+void	update_player_coordinates(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < game->map_rows)
+	{
+		x = 0;
+		while (x > game->map_cols)
+		{
+			if (game->map[y][x] == 'P')
+			{
+				game->pos_x = x;
+				game->pos_y = y;
+				break ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
