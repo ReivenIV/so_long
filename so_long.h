0dd49d2ac5297b9756b1dc05245360fd97c50ccc
ReivenIV <runyalen88@duck.com>
2025-02-20 13:53:06 +0100

    .h first steps

---
+++ so_long.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bobytrap <bobytrap@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 13:00:10 by bobytrap          #+#    #+#             */
+/*   Updated: 2025/02/20 13:00:10 by bobytrap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	SO_LONG_H
+# define SO_LONG_H
+
+// libs
+
+# include <stdio.h>									//! printf just for testing 
+# include <unistd.h>								// Write
+# include <stdlib.h>								// Mallocs
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+// Parameters
+# define SIZE 32
+# define HEIGHT 32
+# define WIDTH 32
+
+// Assets
+
+//	------------------------
+//	::  Arrow parameters  ::
+//	------------------------
+# define UP 65362
+# define LEFT 65361
+# define DOWN 65364
+# define RIGHT 65363
+
+//	------------------
+//	::  Validators  ::
+//	------------------
+
+
+//	-------------
+//	::  Tools  ::
+//	-------------
+
+//	------------
+//	::  Free  ::
+//	------------
+
+
+
+#endif
