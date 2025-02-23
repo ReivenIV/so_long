d0b479376b529f8805f9e5685a1b1f1fc25f249f
ReivenIV <runyalen88@duck.com>
2025-02-23 11:10:38 +0100

    strlen erased from so_long we will use libft

--- src/tools_str.c
+++ src/tools_str.c
@@ -12,12 +12,12 @@
 
 #include "so_long.h"
 
-int	ft_strlen(char *str)
-{
-	int count;
+// // int	ft_strlen(char *str)
+// // {
+// // 	int count;
 
-	count = 0;
-	while (str[count] != '\0')
-		count++;
-	return (count);
-}
+// // 	count = 0;
+// // 	while (str[count] != '\0')
+// // 		count++;
+// // 	return (count);
+// // }
