bcee2d85ce0b4899b3984e8ed0807251cdce1ad7
ReivenIV <runyalen88@duck.com>
2025-02-23 13:48:16 +0100

    init_map created

--- src/tools_inits.c
+++ src/tools_inits.c
@@ -36,8 +36,48 @@ void	init_struct_game(t_game *game)
 	game->amount_c = 0;
 	game->collected = 0;
 	game->moves = 0;
-	// // game->exit = 0;
-	// // game->wall = 0;
-	// // game->background = 0;
 }
 
+int	init_map(char *av, t_game *game)
+{
+	char	*line;
+	int		i;
+	int		fd;
+
+	i = 0;
+	line = 0;
+	game->map = malloc(sizeof(char *) * (game->amount_rows + 1));
+	if (!game->map)
+		return (0);
+	fd = open(av, O_RDONLY);
+	if (fd == -1)
+		return (free(game->map), exit(1), 0);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		game->map[i] = ft_strtrim(line, "\n");
+		free(line);
+		if (!game->map[i])
+				return (free_map(game->map, game), close(fd), 0);
+		i++;
+	}
+	game->map[i] = NULL;
+	close(fd);
+	return (1);
+}
+// Test init_map
+// // int main(int ac, char **av)
+// // {
+// // 	(void)ac;
+// //     t_game game;
+// //     init_struct_game(&game);
+// //     game.amount_rows = count_lines(av[1]);
+// //     if (game.amount_rows == -1)
+// //     {
+// //         perror("Error counting lines in file");
+// //         return 1;
+// //     }
+// //     init_map(av[1], &game);
+// // 	print_map(game.map, game.amount_rows);
+// //     free_map(game.map, &game);
+// //     return (0);
+// // }
