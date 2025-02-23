d3146652072717d7bf70c5fee1cd53680346dbe1
ReivenIV <runyalen88@duck.com>
2025-02-23 14:09:23 +0100

    Main to test flood_fill with own functions

--- src/tools_map.c
+++ src/tools_map.c
@@ -45,7 +45,6 @@ int	flood_fill(t_game *game, char **map, int x, int y)
 	static int	collected_c;
 	static int	exits;
 
-	////print_game_struct(game);
 	if (y < 0 || x < 0 || y > game->amount_rows || x > game->amount_cols || map[y][x] == '1' || map[y][x] == 'X')		// Limits of the flood fill
 		return (0);
 	if (map[y][x] == 'E')
@@ -66,27 +65,37 @@ int	flood_fill(t_game *game, char **map, int x, int y)
 	else
 		return (0);
 }
-
-// // int main()
+// Main to test flood_fill
+// // int main(int argc, char **argv)
 // // {
 // //     t_game game;
+
+// //     if (argc != 2)
+// //     {
+// //         printf("Usage: %s <map_file>\n", argv[0]);
+// //         return 1;
+// //     }
 // //     init_struct_game(&game);
-// //     game.amount_rows = 5;
-// //     game.amount_cols = 5;
-// //     game.amount_c = 1;
-
-// //     char *map[5] = {
-// //         "11111",
-// //         "1C0E1",
-// //         "10001",
-// //         "1P001",
-// //         "11111"
-// //     };
-
-// //     printf("test\n");
-
-// //     int result = flood_fill(&game, map, 1, 1);
+// //     game.amount_rows = count_lines(argv[1]);
+// //     if (game.amount_rows == -1)
+// //     {
+// //         perror("Error counting lines in file");
+// //         return 1;
+// //     }
+// //     if (!init_map(argv[1], &game))
+// //     {
+// //         perror("Error initializing map");
+// //         return 1;
+// //     }
+// //     game.amount_cols = strlen(game.map[0]); // Set the amount_cols based on the first row length
+// //     game.amount_c = 2;
+// //     update_player_coordinates(&game);
+// // 	printf("x: %i\ny: %i\n", game.coor_x, game.coor_y);
+// //     print_map(game.map, game.amount_rows);
+// //     printf("Starting flood fill at: (%d, %d)\n", game.coor_x, game.coor_y);
+// //     int result = flood_fill(&game, game.map, game.coor_x, game.coor_y);
 // //     printf("Flood fill result: %d\n", result);
-
+// //     print_map(game.map, game.amount_rows);
+// //     free_map(game.map, &game);
 // //     return 0;
 // // }
