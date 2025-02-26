ab22fbe08e6c0e928a8a8a01a0ddb2812c902e50
rita <rita.email@contact.me>
2025-02-26 18:22:51 +0100

    tools_events.c created TODO

---
+++ src/tools_events.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_events.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bobydear <bobydear@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 17:18:21 by bobydear          #+#    #+#             */
+/*   Updated: 2025/02/26 17:58:18 by bobydear         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+// Missing external functions
+int	handler_events_keys(int	key, t_game *game)
+{
+	// // if (key == XK_Escape)
+	// // 	handler_exit_game(game);
+	//// else if (key == XK_w || key == XK_Left)
+		
+}
