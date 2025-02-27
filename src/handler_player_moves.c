66b49542a1e92a26f164517974750d4ce58abb4c
rita <rita.email@contact.me>
2025-02-27 13:52:11 +0100

    handler_player_moves.c created

---
+++ src/handler_player_moves.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handler_player_moves.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bobydear <bobydear@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 13:29:50 by bobydear          #+#    #+#             */
+/*   Updated: 2025/02/27 13:32:34 by bobydear         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+// Will call tools_player_moves.c & tools_player.C
+/*
+Will manage during the game : 
+	- All images updates, 
+	- game->map updates, 
+	- key events 
+	- IFs the player can or not move in a certain direction.  
+*/
+void	handler_player_moves(char key, t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = game->coor_x;
+	y = game->coor_y;
+	if (key == 'W' && is_next_position_valid(game, x, (y - 1)) == 1)
+		move(game, 'W', x, y);
+	if (key == 'A' && is_next_position_valid(game, (x - 1), y) == 1)
+		move(game, 'A', x, y);
+	if (key == 'S' && is_next_position_valid(game, x, (y + 1)) == 1)
+		move(game, 'S', x, y);
+	if (key == 'D' && is_next_position_valid(game, (x + 1), y) == 1)
+		move(game, 'D', x, y);
+	return ;
+}
