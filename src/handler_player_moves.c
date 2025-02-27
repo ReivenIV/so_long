da3fd657997f3a636214422629e718a723eb3dac
rita <rita.email@contact.me>
2025-02-27 16:46:34 +0100

    handler_player_moves fixbug

--- src/handler_player_moves.c
+++ src/handler_player_moves.c
@@ -6,7 +6,7 @@
 /*   By: bobydear <bobydear@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:29:50 by bobydear          #+#    #+#             */
-/*   Updated: 2025/02/27 16:03:13 by bobydear         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:38:58 by bobydear         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,19 @@ int	handler_player_moves(int key, void *param)
 	int	x;
 	int	y;
 	t_game	*game;
-	
+
 	game = (t_game *)param;
 	x = game->coor_x;
 	y = game->coor_y;
-	if (key == 'W' && is_next_position_valid(game, x, (y - 1)) == 1)
-		move(game, 'W', x, y);
-	if (key == 'A' && is_next_position_valid(game, (x - 1), y) == 1)
-		move(game, 'A', x, y);
-	if (key == 'S' && is_next_position_valid(game, x, (y + 1)) == 1)
-		move(game, 'S', x, y);
-	if (key == 'D' && is_next_position_valid(game, (x + 1), y) == 1)
-		move(game, 'D', x, y);
+	if (key == XK_Escape)
+		handler_exit_game(game);
+	else if ((key == XK_w || key == XK_Up) && is_next_pos_ok(game, x, (y - 1)) == 1)
+		move_player(game, 'W', x, y);
+	else if ((key == XK_a || key == XK_Left) && is_next_pos_ok(game, (x - 1), y) == 1)
+		move_player(game, 'A', x, y);
+	else if ((key == XK_s || key == XK_Down) && is_next_pos_ok(game, x, (y + 1)) == 1)
+		move_player(game, 'S', x, y);
+	else if ((key == XK_d || key == XK_Right) && is_next_pos_ok(game, (x + 1), y) == 1)
+		move_player(game, 'D', x, y);
 	return (0);
 }
